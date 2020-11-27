4f31ca65934c1b1d7a796f3406a3b61fe76e015b
19034561 <mamingyang@haier.com>
2020-11-27 11:26:30 +0800

    specification code-influx

--- app-starter-influx/src/main/java/com/cosmoplat/influx/config/YamlConfigFactory.java
+++ app-starter-influx/src/main/java/com/cosmoplat/influx/config/YamlConfigFactory.java
@@ -14,23 +14,23 @@ import java.util.Properties;
  */
 public class YamlConfigFactory extends DefaultPropertySourceFactory {
 
-    @Override
-    public PropertySource<?> createPropertySource(String name, EncodedResource resource) throws IOException {
-        String sourceName = name != null ? name : resource.getResource().getFilename();
-        if (!resource.getResource().exists()) {
-            return new PropertiesPropertySource(sourceName, new Properties());
-        } else if (sourceName.endsWith(".yml") || sourceName.endsWith(".yaml")) {
-            Properties propertiesFromYaml = loadYml(resource);
-            return new PropertiesPropertySource(sourceName, propertiesFromYaml);
-        } else {
-            return super.createPropertySource(name, resource);
-        }
+  @Override
+  public PropertySource<?> createPropertySource(String name, EncodedResource resource) throws IOException {
+    String sourceName = name != null ? name : resource.getResource().getFilename();
+    if (!resource.getResource().exists()) {
+      return new PropertiesPropertySource(sourceName, new Properties());
+    } else if (sourceName.endsWith(".yml") || sourceName.endsWith(".yaml")) {
+      Properties propertiesFromYaml = loadYml(resource);
+      return new PropertiesPropertySource(sourceName, propertiesFromYaml);
+    } else {
+      return super.createPropertySource(name, resource);
     }
+  }
 
-    private Properties loadYml(EncodedResource resource) throws IOException {
-        YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
-        factory.setResources(resource.getResource());
-        factory.afterPropertiesSet();
-        return factory.getObject();
-    }
+  private Properties loadYml(EncodedResource resource) throws IOException {
+    YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
+    factory.setResources(resource.getResource());
+    factory.afterPropertiesSet();
+    return factory.getObject();
+  }
 }
