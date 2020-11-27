2eceb96afe6c22f970c8587ca075d20d8c3e6e7b
Robben <19931213>
2020-11-27 14:08:17 +0800

    规范

--- app-starter-redis/src/main/java/com/cosmoplat/redis/util/RedisUtil.java
+++ app-starter-redis/src/main/java/com/cosmoplat/redis/util/RedisUtil.java
@@ -479,8 +479,9 @@ public class RedisUtil {
     public boolean lSet(String key, Object value, long time) {
         try {
             redisTemplate.opsForList().rightPush(key, value);
-            if (time > 0)
-                expire(key, time);
+            if (time > 0) {
+              expire(key, time);
+            }
             return true;
         } catch (Exception e) {
             e.printStackTrace();
@@ -516,8 +517,9 @@ public class RedisUtil {
     public boolean lSet(String key, List<Object> value, long time) {
         try {
             redisTemplate.opsForList().rightPushAll(key, value);
-            if (time > 0)
-                expire(key, time);
+            if (time > 0) {
+              expire(key, time);
+            }
             return true;
         } catch (Exception e) {
             e.printStackTrace();
