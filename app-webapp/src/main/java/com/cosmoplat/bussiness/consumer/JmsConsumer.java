c22ded16f3d9aa775e3e7fc567628ba60b72853e
19034561 <mamingyang@haier.com>
2020-11-30 14:23:25 +0800

    优化

--- app-webapp/src/main/java/com/cosmoplat/bussiness/consumer/JmsConsumer.java
+++ app-webapp/src/main/java/com/cosmoplat/bussiness/consumer/JmsConsumer.java
@@ -28,35 +28,35 @@ public class JmsConsumer {
 
     @JmsListener(destination = "topickkk")
     public void receiveTopic(String message) throws IOException, EncodeException {
-        if (null != message) {
-            String messageString = message;
-            log.info("------------获取到边缘层信息-----------：" + messageString);
-            //json数据解析
-            JSONObject jsonObject = JSONObject.parseObject(messageString);
-            List<Map<String, Object>> listData = (List<Map<String, Object>>) jsonObject.get("data");
-          if (!CollectionUtils.isEmpty(listData)) {
-            Map<String, String> tags = new HashMap<>(16);
-            Map<String, Object> fields = new HashMap<>(16);
-            for (int i = 0; i < listData.size(); i++) {
-              String machineNumber = String.valueOf(listData.get(i).get("machineNumber"));
-              Map<String, Object> map = (Map<String, Object>) listData.get(i).get("message");
-              String n = JSONObject.toJSONString(map);
-              //封装时序数据库参数
-              tags.put("machineNumber", machineNumber);
-              fields.put("message", n);
-              influxDBUtils.insert("machine_info", tags, fields, System.currentTimeMillis(), TimeUnit.MILLISECONDS);
-              log.info("-------------插入时序数据库-----------tag:" + machineNumber);
-            }
-            if (WebSocketServer.getOnlineCount() != 0) {
-              String machineNumeber = AssetHealthController.getMachineNumber();
-              String queryCommand = "SELECT * FROM \"machine_info\"  where machineNumber='" + machineNumeber + "' order by time desc limit 10";
-              QueryResult result = influxDBUtils.query(queryCommand);
-              String resultString = JSONObject.toJSONString(result);
-              WebSocketServer.sendInfo(resultString);
-            }
+      if (null != message) {
+        String messageString = message;
+        log.info("------------获取到边缘层信息-----------：" + messageString);
+        //json数据解析
+        JSONObject jsonObject = JSONObject.parseObject(messageString);
+        List<Map<String, Object>> listData = (List<Map<String, Object>>) jsonObject.get("data");
+        if (!CollectionUtils.isEmpty(listData)) {
+          Map<String, String> tags = new HashMap<>(16);
+          Map<String, Object> fields = new HashMap<>(16);
+          for (int i = 0; i < listData.size(); i++) {
+            String machineNumber = String.valueOf(listData.get(i).get("machineNumber"));
+            Map<String, Object> map = (Map<String, Object>) listData.get(i).get("message");
+            String n = JSONObject.toJSONString(map);
+            //封装时序数据库参数
+            tags.put("machineNumber", machineNumber);
+            fields.put("message", n);
+            influxDBUtils.insert("machine_info", tags, fields, System.currentTimeMillis(), TimeUnit.MILLISECONDS);
+            log.info("-------------插入时序数据库-----------tag:" + machineNumber);
           }
-        } else {
-            log.info("------------获取到边缘层信息为空-----------");
+          if (WebSocketServer.getOnlineCount() != 0) {
+            String machineNumeber = AssetHealthController.getMachineNumber();
+            String queryCommand = "SELECT * FROM \"machine_info\"  where machineNumber='" + machineNumeber + "' order by time desc limit 10";
+            QueryResult result = influxDBUtils.query(queryCommand);
+            String resultString = JSONObject.toJSONString(result);
+            WebSocketServer.sendInfo(resultString);
+          }
         }
+      } else {
+        log.info("------------获取到边缘层信息为空-----------");
+      }
     }
 }
