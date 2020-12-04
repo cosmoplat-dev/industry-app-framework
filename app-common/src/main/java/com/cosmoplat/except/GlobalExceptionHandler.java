127eafc8eee0b7305a926d6eee4ebb2aec8ade6d
Robben <19931213>
2020-12-04 13:57:36 +0800

    增加工具类

---
+++ app-common/src/main/java/com/cosmoplat/except/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.cosmoplat.except;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @author Willard Hu on 2019-04-29.
+ */
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // TODO 其他异常类型处理
+}
