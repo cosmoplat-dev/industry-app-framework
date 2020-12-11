59474d67d4bbb43254d6b6f8c236945f5823e3f5
haoxuexin <haoxuexin@haier.com>
2020-12-11 17:50:19 +0800

    code format

--- app-common/src/main/java/com/cosmoplat/util/SpringUtils.java
+++ app-common/src/main/java/com/cosmoplat/util/SpringUtils.java
@@ -9,18 +9,18 @@ import org.springframework.stereotype.Component;
 
 /**
  * spring工具类 方便在非spring管理环境中获取bean
- * 
+ *
  * @author yyf
  */
 @Component
-public final class SpringUtils implements BeanFactoryPostProcessor
-{
-    /** Spring应用上下文环境 */
-    private static ConfigurableListableBeanFactory beanFactory;
+public final class SpringUtils implements BeanFactoryPostProcessor {
+  /**
+   * Spring应用上下文环境
+   */
+  private static ConfigurableListableBeanFactory beanFactory;
 
     @Override
-    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException
-    {
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
         SpringUtils.beanFactory = beanFactory;
     }
 
@@ -33,8 +33,7 @@ public final class SpringUtils implements BeanFactoryPostProcessor
      *
      */
     @SuppressWarnings("unchecked")
-    public static <T> T getBean(String name) throws BeansException
-    {
+    public static <T> T getBean(String name) throws BeansException {
         return (T) beanFactory.getBean(name);
     }
 
@@ -46,8 +45,7 @@ public final class SpringUtils implements BeanFactoryPostProcessor
      * @throws org.springframework.beans.BeansException
      *
      */
-    public static <T> T getBean(Class<T> clz) throws BeansException
-    {
+    public static <T> T getBean(Class<T> clz) throws BeansException {
         T result = (T) beanFactory.getBean(clz);
         return result;
     }
@@ -71,8 +69,7 @@ public final class SpringUtils implements BeanFactoryPostProcessor
      * @throws org.springframework.beans.factory.NoSuchBeanDefinitionException
      *
      */
-    public static boolean isSingleton(String name) throws NoSuchBeanDefinitionException
-    {
+    public static boolean isSingleton(String name) throws NoSuchBeanDefinitionException {
         return beanFactory.isSingleton(name);
     }
 
@@ -82,8 +79,7 @@ public final class SpringUtils implements BeanFactoryPostProcessor
      * @throws org.springframework.beans.factory.NoSuchBeanDefinitionException
      *
      */
-    public static Class<?> getType(String name) throws NoSuchBeanDefinitionException
-    {
+    public static Class<?> getType(String name) throws NoSuchBeanDefinitionException {
         return beanFactory.getType(name);
     }
 
@@ -95,14 +91,13 @@ public final class SpringUtils implements BeanFactoryPostProcessor
      * @throws org.springframework.beans.factory.NoSuchBeanDefinitionException
      *
      */
-    public static String[] getAliases(String name) throws NoSuchBeanDefinitionException
-    {
+    public static String[] getAliases(String name) throws NoSuchBeanDefinitionException {
         return beanFactory.getAliases(name);
     }
 
     /**
      * 获取aop代理对象
-     * 
+     *
      * @param invoker
      * @return
      */
