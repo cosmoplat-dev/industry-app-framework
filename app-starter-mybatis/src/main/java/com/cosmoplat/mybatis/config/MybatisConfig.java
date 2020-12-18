3777d8565ee0a00a79f6a2b9844f64f275b35cc1
Robben <19931213>
2020-12-18 14:58:12 +0800

    规范

--- app-starter-mybatis/src/main/java/com/cosmoplat/mybatis/config/MybatisConfig.java
+++ app-starter-mybatis/src/main/java/com/cosmoplat/mybatis/config/MybatisConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.PropertySource;
  * @author 27279
  */
 @Configuration
-@PropertySource(value = { "classpath:application-mybatis.yml",
-}, encoding = "utf-8",factory = YamlConfigFactory.class)
+@PropertySource(value = { "classpath:application-mybatis.yml",}, encoding = "utf-8",factory = YamlConfigFactory.class)
 public class MybatisConfig {
 }
